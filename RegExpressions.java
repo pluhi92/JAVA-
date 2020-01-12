fa952444353d49f6374a8b0cba9883891a97ae93
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-01-12 14:44:27 +0100

    Update RegExpressions.java

--- RegExpressions.java
+++ RegExpressions.java
@@ -12,3 +12,21 @@ public class RegExpressions {
            System.out.println(Arrays.toString(parts));
         }
     }
+
+//////////////////////////////////////////////////////////////////////////
+
+
+import java.util.Arrays;
+
+public class RegExpressions2 {
+
+
+        public static void main(String[] args) {
+
+            var msg = "Today is a beatiful day";
+
+           String[] parts = msg.split("\\s{1,9}");
+
+           System.out.println(Arrays.toString(parts));
+        }
+    }
