e30e9153250b51df76836173213636743e35252b
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-01-12 15:06:37 +0100

    Create ImportData.java

---
+++ ImportData.java
@@ -0,0 +1,21 @@
+public class ImportData {
+
+
+    public static void main(String[] args) throws IOException {
+
+        var path = Paths.get("src/resources/data.txt");
+        String content = Files.readString(path);
+
+        String[] parts = content.split("[,\\s]{1,3}");
+
+        int sum = 0;
+
+        for (String part : parts) {
+
+            sum += Integer.parseInt(part);
+
+        }
+            System.out.println(sum);
+
+    }
+}
