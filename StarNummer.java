da0d6856c9e769aecc56d652ca7af6f7a8225d60
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-01-18 12:03:59 +0100

    Update StarNummer.java

--- StarNummer.java
+++ StarNummer.java
@@ -15,3 +15,25 @@ public class StarNummer {
         }
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////
+package com.zetcode;
+
+
+public class StarNummer2 {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {7, 9, 2, 6, 12, 10};
+
+        String unit = "*";
+
+        for (var num : nums) {
+            for (int i = 0; i < num; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
