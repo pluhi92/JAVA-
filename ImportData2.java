8d688666b65154b57dc8eac3a289cf3a467baf90
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-01-18 16:49:46 +0100

    Update ImportData2.java

--- ImportData2.java
+++ ImportData2.java
@@ -23,3 +23,35 @@ public class ImportData2 {
 
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.zetcode;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import java.util.List;
+
+// latin characters only
+
+public class SimpleEx2 {
+
+    public static void main(String[] args) throws IOException {
+
+        var path = Paths.get("src/resources/words.txt");
+
+        List<String> lines = Files.readAllLines(path);
+
+        //lines.forEach(line -> System.out.println(line));
+        //System.out.println("**********************************");
+
+        lines.forEach(System.out::println);
+
+    }
+        private static void upperWords(String word) {
+
+            System.out.println(word.toUpperCase());
+        }
+}
