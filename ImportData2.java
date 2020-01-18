df974a268faca7e60a60177636c0c7fd99539590
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-01-18 16:52:01 +0100

    Update ImportData2.java

--- ImportData2.java
+++ ImportData2.java
@@ -55,3 +55,35 @@ public class SimpleEx2 {
             System.out.println(word.toUpperCase());
         }
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.zetcode;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import java.util.List;
+
+// latin characters only
+
+public class SimpleEx2 {
+
+    public static void main(String[] args) throws IOException {
+
+        var path = Paths.get("src/resources/words.txt");
+
+        List<String> lines = Files.readAllLines(path);
+
+        //lines.forEach(line -> System.out.println(line));
+        //System.out.println("**********************************");
+
+        lines.forEach(SimpleEx2::upperWords);
+        lines.forEach(item -> upperWords(item));
+
+    }
+        private static void upperWords(String word) {
+
+            System.out.println(word.toUpperCase());
+        }
+}
