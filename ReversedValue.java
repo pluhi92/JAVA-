0fcc17b946d1c3f6b3166720aa58c12a5693e7c7
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-01-18 17:27:46 +0100

    Update ReversedValue.java

--- ReversedValue.java
+++ ReversedValue.java
@@ -3,8 +3,6 @@ package com.zetcode;
 import java.io.IOException;
 
 
-
-
 public class ReversedValue {
 
     public static void main(String[] args) throws IOException {
@@ -21,5 +19,39 @@ public class ReversedValue {
         System.out.println(reversedValue);
 
 
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+package com.zetcode;
+
+import java.io.IOException;
+
+
+public class ReversedValue2 {
+
+    public static void main(String[] args) throws IOException {
+
+        int value = 23456;
+
+        String str_value = Integer.toString(value);
+
+        char[] elements = str_value.toCharArray();
+
+        char[] reversed_elements = new char[elements.length];
+
+       // reversed_elements[0] = elements[elements.length-1];
+      //  reversed_elements[1] = elements[elements.length-2];
+
+            for (int i = elements.length - 1, j=0; i>=0 ; i--, j++) {
+                reversed_elements[j] = elements[i];
+
+            }
+
+            String reversedStr = new String(reversed_elements);
+            int rev_value = Integer.parseInt(reversedStr);
+
+            System.out.println(rev_value);
     }
 }
