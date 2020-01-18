10e4f50ee62fe3d88eba2cc06e3b45774d76977c
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-01-18 12:31:38 +0100

    Update CycleFor.java

--- CycleFor.java
+++ CycleFor.java
@@ -18,3 +18,30 @@ public class CycleFor {
 
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.zetcode;
+
+
+public class CzcleFor2 {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {7, 9, 2, 6, 12, 10};
+
+        String[] items = {"beers", "coins", "pens", "pencils", "rocks", "cars"};
+
+        int i = 0;
+
+        for (var num: nums) {
+
+            System.out.printf("There are %d %s%n", nums[i], items[i]);
+
+            i++;
+
+        }
+
+    }
+}
