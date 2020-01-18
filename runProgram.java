a23542106c7163b1fe3ad1ab956d40fb79f691a6
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-01-18 11:33:22 +0100

    Create runProgram.java

---
+++ runProgram.java
@@ -0,0 +1,14 @@
+package com.zetcode;
+
+import java.io.IOException;
+
+
+public class runProgram {
+
+
+    public static void main(String[] args) throws IOException {
+
+      Runtime.getRuntime().exec("notepad.exe");
+      
+    }
+}
