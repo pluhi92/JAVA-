47f180760c6914634855c3c635a1e39b983c563c
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-01-18 17:04:55 +0100

    Update ImportData2.java

--- ImportData2.java
+++ ImportData2.java
@@ -65,9 +65,8 @@ import java.nio.file.Paths;
 
 import java.util.List;
 
-// latin characters only
 
-public class SimpleEx2 {
+public class ImportData2 {
 
     public static void main(String[] args) throws IOException {
 
@@ -86,4 +85,36 @@ public class SimpleEx2 {
 
             System.out.println(word.toUpperCase());
         }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.zetcode;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
+
+public class ImportData2 {
+
+    public static void main(String[] args) throws IOException {
+
+        //var path = Paths.get("src/resources/words.txt");
+
+        // List<String> lines = Files.readAllLines(path);
+
+        try (var br = new BufferedReader(new FileReader("src/resources/words.txt",
+                StandardCharsets.UTF_8))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                System.out.println(line);
+            }
+        }
+
+    }
 }
