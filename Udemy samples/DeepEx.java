26384be2e89859a623418a9556b70fc38cd14959
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-06-05 15:16:19 +0200

    Create DeepEx.java

---
+++ Udemy samples/DeepEx.java
@@ -0,0 +1,34 @@
+package com.zetcode;
+
+public class DeepEx {
+    public static void main(String[] args) {
+
+        Abc obj = new Abc();
+
+        obj.i = 5;
+        obj.j = 6;
+
+        Abc obj1 = new Abc();
+
+        obj1.i = obj.i;
+        obj1.j = obj.j;
+
+        obj1.j = 8;
+
+        System.out.println(obj1);
+        System.out.println(obj);
+    }
+}
+
+class Abc {
+    int i;
+    int j;
+
+    @Override
+    public String toString() {
+        return "Abc{" +
+                "i=" + i +
+                ", j=" + j +
+                '}';
+    }
+}
