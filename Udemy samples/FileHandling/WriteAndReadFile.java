5bec58f2f6a43aa131eb5db1b80887b54cfdaf8b
pluhi92 <59772641+pluhi92@users.noreply.github.com>
2020-06-17 15:23:02 +0200

    Update WriteAndReadFile.java

--- Udemy samples/FileHandling/WriteAndReadFile.java
+++ Udemy samples/FileHandling/WriteAndReadFile.java
@@ -19,3 +19,25 @@ public class WriteAndReadFile {
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.zetcode;
+
+import java.io.*;
+
+public class TestEx {
+    public static void main(String[] args) throws Exception {
+
+        File f = new File("pluhi.txt");   //this method simplifies the process
+
+        FileOutputStream fos = new FileOutputStream(f); //just enter "f" 
+        DataOutputStream dos = new DataOutputStream(fos);
+        dos.writeUTF("This is Spartaaaaa!!!");
+
+        FileInputStream fis = new FileInputStream(f); //here also just "f"
+        DataInputStream dis = new DataInputStream(fis);
+        String str = dis.readUTF();
+        System.out.println(str);
+    }
+}
+
